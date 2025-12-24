80beff495ab39cd5d27a960d9e879241c1966d75
Kamal <atluxanovvkamal@gmail.com>
2025-12-24 11:50:07 +0000

    linux

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$DOMAIN" | awk -F': ' '
+whois $1 | awk -F: '/Registrant|Admin|Tech/ {gsub(/^ /,"",$2); name=$1; val=$2; if(name~/Street/) val=val" "; if(name~/Phone$/) {print name","val; print name" Ext:,"} else if(name~/Fax$/) {print name","val; print name" Ext:,"} else print name","val}' > $1.csv
