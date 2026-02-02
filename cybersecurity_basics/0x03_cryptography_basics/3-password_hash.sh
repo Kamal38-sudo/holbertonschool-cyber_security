355ade960ab4e7e92ba5aa5a9ca8d7ae08ba0328
Kamal38-sudo <atluxanovvkamal@gmail.com>
2026-02-02 11:12:52 -0500

    Cryptography

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-SALT=$(openssl rand -base64 12 | cut -c1-16)
-echo -n "$1$SALT" | openssl dgst -sha512 > 3_hash.txt
+SALT=$(openssl rand -base64 12 | cut -c1-16) | echo -n "$1$SALT" | openssl dgst -sha512 > 3_hash.txt
