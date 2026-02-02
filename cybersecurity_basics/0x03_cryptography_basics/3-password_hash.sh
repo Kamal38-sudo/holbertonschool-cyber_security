a93e08f6077ce666f9c0ceb65930919bfd9e684d
Kamal38-sudo <atluxanovvkamal@gmail.com>
2026-02-02 11:12:18 -0500

    Cryptography

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Generate random 16-character salt
 SALT=$(openssl rand -base64 12 | cut -c1-16)
-# Combine password with salt and hash using SHA-512
 echo -n "$1$SALT" | openssl dgst -sha512 > 3_hash.txt
